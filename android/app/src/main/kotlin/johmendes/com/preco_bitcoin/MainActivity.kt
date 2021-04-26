c705dd0fbce41f17446e8bade9090d87db85f207
johmendes <johmendes.com.jokenpo>
2021-04-26 12:52:16 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/johmendes/com/preco_bitcoin/MainActivity.kt
@@ -0,0 +1,6 @@
+package johmendes.com.preco_bitcoin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Home.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+    String _preco = "0";
+
+    void _recuperaPreco() async {
+
+      String url = "https://blockchain.info/ticker";
+      http.Response response = await http.get(url);
+
+      Map<String, dynamic> retorno = json.decode( response.body);
+      setState(() {
+        _preco = retorno["BRL"] ["buy"].toString();
+      });
+
+      print("Resultado :" + retorno["BRL"] ["buy"].toString());
+    }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("imagens/bitcoin.png"),
+            Padding(padding: EdgeInsets.only(top: 30, bottom: 30),
+             child: Text(
+                "R\$" + _preco,
+                style: TextStyle(
+                    fontSize: 35
+                ),
+              ),
+            ),
+            RaisedButton(
+                child: Text(
+                    "Atualizar",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+                ),
+                color: Colors.orange,
+                padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                onPressed: _recuperaPreco,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
